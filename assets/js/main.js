976a00432a7a431deaee25976a82bb7a3176831a
dionisioR <72356766+dionisioR@users.noreply.github.com>
2024-11-26 21:00:37 -0300

    Update 2024-11-26
    Update 2024-11-26

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,34 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+function convertPokemonToLi(pokemon) {
+  return `
+        <li class="pokemon">
+          <span class="number">#001</span>
+          <span class="name">${pokemon.name}</span>
+          <div class="detail">
+            <ol class="types">
+              <li class="type">grass</li>
+              <li class="type">poison</li>
+            </ol>
+            <img class=""
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+              alt="${pokemon.name}"
+            />
+          </div>
+        </li>
+  `;
+}
 
-fetch(url)
-  .then((response) => response.json())
-  .then((responseBody) => console.log(responseBody))
-  .catch((error) => console.log(error))
-  .finally(() => console.log("Requisição finalizada!"));
+const pokemonList = document.querySelector("#pokemonList");
+
+pokeApi.getPokemons().then((pokemons = []) => {
+
+  // const newList = pokemons.map(
+  //   (pokemon) =>  convertPokemonToLi(pokemon)
+  // )
+  // const newHtml = newList.join('')
+  // pokemonList.innerHTML = newHtml
+
+  // Posso trocar o código acima por:
+  pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('')
+
+});
+ 
